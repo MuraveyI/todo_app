fab3ad57775dea4fe716db7bd1e45468ac51bc70
Aya <shalkaraya99@gmail.com>
2019-01-27 19:57:22 +0600

    add task model

---
+++ app/src/main/java/com/example/macbookair/myapplication/Models/Task.java
@@ -0,0 +1,15 @@
+package com.example.macbookair.myapplication.Models;
+
+import java.util.Date;
+
+public class Task {
+
+
+    String title;
+    String description;
+    Boolean iaDone;
+    Date creationDate;
+    Date endDate;
+
+
+}
