647bfa7c6c7274a66865f5c4a82b302ef63e62e9
Aya <shalkaraya99@gmail.com>
2019-01-27 21:05:39 +0600

    add tasks adapter

---
+++ app/src/main/java/com/example/macbookair/myapplication/Adapters/TasksAdapter.java
@@ -0,0 +1,46 @@
+package com.example.macbookair.myapplication.Adapters;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.macbookair.myapplication.Models.Task;
+import com.example.macbookair.myapplication.R;
+import com.example.macbookair.myapplication.Views.TaskViewHolder;
+
+import java.util.List;
+
+public class TasksAdapter extends RecyclerView.Adapter<TaskViewHolder> {
+
+    List<Task> tasks;
+
+    public TasksAdapter(List<Task>tasks){
+        this.tasks=tasks;
+
+
+    }
+
+    @NonNull
+    @Override
+    public TaskViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.view_holder_task, viewGroup,false);
+        TaskViewHolder vh =new TaskViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TaskViewHolder taskViewHolder, int i) {
+        taskViewHolder.onBind(tasks.get(i));
+
+
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return tasks.size();
+    }
+}
